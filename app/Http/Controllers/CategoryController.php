0186bd22038950864c51ea43fdd3a2959d6c4032
RizallGos <rizaldewaw@gmail.com>
2025-05-03 20:57:32 +0700

    crud resource

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,68 +1,70 @@
 <?php
- 
- namespace App\Http\Controllers;
- 
- use App\Models\Category;
- use Illuminate\Http\Request;
- 
- class CategoryController extends Controller
- {
-     /**
-      * Display a listing of the resource.
-      */
-     public function index()
-     {
-         return view('category');
-         $Categories = Category::all();
-         return view('Category', compact('categories'));
-     }
- 
-     /**
-      * Show the form for creating a new resource.
-      */
-     public function create()
-     {
-         //
-     }
- 
-     /**
-      * Store a newly created resource in storage.
-      */
-     public function store(Request $request)
-     {
-         //
-     }
- 
-     /**
-      * Display the specified resource.
-      */
-     public function show(Category $category)
-     {
-         //
-     }
- 
-     /**
-      * Show the form for editing the specified resource.
-      */
-     public function edit(Category $category)
-     {
-         //
-     }
- 
-     /**
-      * Update the specified resource in storage.
-      */
-     public function update(Request $request, Category $category)
-     {
-         //
-     }
- 
-     /**
-      * Remove the specified resource from storage.
-      */
-     public function destroy(Category $category)
-     {
-         //
-     }
- }
- 
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class CategoryController extends Controller
+{
+    public function index(): View
+    {
+        $categories = Category::all();
+        return view('category', compact('categories')); // View index-nya bernama category.blade.php
+    }
+
+    public function create(): View
+    {
+        return view('create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required|min:3|max:255'
+        ],[
+
+            'name.required' => 'Di isi ya ganteng',
+            'name.min' => 'Minimal 2 karakter',
+            'name.max' => 'Maksimal 255 karakter yh'
+        ]);
+        Category::create([
+            'name' => $request->name
+        ]);
+
+        return redirect()->route('category.index')->with('add', 'Kategori berhasil ditambahkan!');
+    }
+
+    public function edit(Category $category): View
+    {
+        return view('edit', compact('category'));
+    }
+
+    public function update(Request $request, Category $category): RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required|min:3|max:255'
+        ],[
+
+            'name.required' => 'Di isi ya ganteng',
+            'name.min' => 'Minimal 2 karakter',
+            'name.max' => 'Maksimal 255 karakter yh'
+        ]);
+
+        $category->update([
+            'name' => $request->name
+        ]);
+
+        return redirect()->route('category.index')->with('edit', 'Category berhasil diperbarui!');
+    }
+
+    public function destroy(Category $category): RedirectResponse
+    {
+        $category->delete();
+        return redirect()->back()->with('success', 'Category berhasil dihapus!'); 
+    }
+}
+//back itu untuk kembali ke halaman sebelumnya
+//route itu untuk kembali ke halaman yang di inginkan/sudah ditentukan
--- database/seeders/itemSeeder.php
+++ database/seeders/itemSeeder.php
@@ -1,30 +1,20 @@
 <?php
  
  namespace Database\Seeders;
+ 
+ use Illuminate\Support\Facades\DB;
  use Illuminate\Database\Console\Seeds\WithoutModelEvents;
  use Illuminate\Database\Seeder;
- use Illuminate\Support\Facades\DB;
- use Illuminate\Support\Facades\Hash;
  use Illuminate\Support\Str;
- use Faker\Factory as Faker;
+ use Illuminate\Support\Facades\Hash;
  
- class itemSeeder extends Seeder
+ class ItemSeeder extends Seeder
  {
      /**
       * Run the database seeds.
       */
      public function run(): void
      {
-         $faker = Faker::create('id_ID');
- 
-         for($i=1; $i <=10; $i++) {
-             DB::table('items')->insert([
-                 'category_id' => $faker->numberBetween(1, 10),
-                 'name' => $faker->words(1, true),
-                 'price' => $faker->numberBetween(5, 10),
-                 'stock' => $faker->numberBetween(5, 10)
-         ]);
-     }
-     
+         //
      }
  }
